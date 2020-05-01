ffad8a16c32b565af2c2a0bc12b50f7ff40360d1
Andrés Quilodrán <andres.caro.q@gmail.com>
2020-05-01 19:17:47 -0400

    Add forward and lateral player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    void Start()
+    {
+        
+    }
+
+    void Update() {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement() {
+        float forwardTranslation = Input.GetAxis("Vertical");
+        float lateralTranslation = Input.GetAxis("Horizontal");
+
+        forwardTranslation *= Time.deltaTime;
+        lateralTranslation *= Time.deltaTime;
+
+        transform.Translate(new Vector3(lateralTranslation, 0, forwardTranslation));
+    }
+}
